7f3421612d40ec16556989d11266d13e17503c6e
Antvarf <antvarf@gmail.com>
2023-03-03 23:32:30 +0300

    refactor: add initial working models version + tests

--- base/models.py
+++ base/models.py
@@ -1,14 +1,14 @@
 import uuid
-from typing import Union
 
-from django.db import models
-from django.db.models import Q
+from django.db import models, transaction
+from django.db.models import Q, Max, Avg, Count
 from django.db.models.constraints import CheckConstraint
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import (
     make_password,
     check_password,
 )
+from django.utils import timezone
 
 
 class GameModes(models.TextChoices):
@@ -21,87 +21,47 @@ class GameModes(models.TextChoices):
 User = get_user_model()
 
 
+class StatsQuerySet(models.QuerySet):
+    def with_stats(self, mode: str = None):
+        if mode is not None:
+            if mode not in GameModes.values:
+                raise ValueError(f'`{mode}` is not a defined gamemode')
+            condition = Q(sessions__session__mode=mode)
+        else:
+            condition = None
+        return self.annotate(
+            best_score=Max('sessions__score', default=0, filter=condition),
+            best_speed=Max('sessions__speed', default=0, filter=condition),
+            avg_score=Avg('sessions__score', default=0, filter=condition),
+            avg_speed=Avg('sessions__speed', default=0, filter=condition),
+            games_played=Count('sessions', filter=condition),
+        )
+
+
 class Player(models.Model):
     """Stores profile data i.e. player stats and displayed name for user"""
     displayed_name = models.CharField(max_length=50)
     user = models.OneToOneField(User, blank=True, null=True,
                                 on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.displayed_name
-
-    def save(self, *args, **kwargs):
-        is_created = self._state.adding
-        super().save(*args, **kwargs)
-        if is_created:
-            Stats.objects.create_player_stats(player=self)
-
-
-class StatsQuerySet(models.QuerySet):
-    def overall(self):
-        return self.filter(mode=None)
-
-    def for_gamemode(self, mode: str):
-        return self.filter(mode=mode)
-
-    def updated_from_result(self, result):
-        for stats in self:
-            stats.update_from_result(score=result.score, speed=result.speed)
-        return self
-
-    def create_player_stats(self, player):
-        modes = GameModes.values
-        stats = [Stats(mode=mode, player=player) for mode in (None, *modes)]
-        return self.bulk_create(stats)
-
-
-class Stats(models.Model):
-    """Stores overall and per-mode stats for each player"""
-    mode: str = models.CharField(max_length=1,
-                                 choices=GameModes.choices, null=True)
-    player: int = models.ForeignKey('Player',
-                                    related_name='stats',
-                                    on_delete=models.CASCADE)
-    avg_score: int = models.IntegerField(default=0)
-    best_score: int = models.IntegerField(default=0)
-    avg_speed: float = models.FloatField(default=0)
-    best_speed: float = models.FloatField(default=0)
-    games_played: int = models.PositiveIntegerField(default=0)
-
     objects = StatsQuerySet.as_manager()
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=('mode', 'player'),
-                name='unique_player_mode_stats',
-            ),
-        ]
-
-    def update_from_result(self, score: int, speed: float) -> None:
-        def new_avg(old_avg: Union[int, float], value: Union[int, float], n: int) -> float:
-            return (old_avg*n + value) / (n + 1)
-
-        self.best_score = max(self.best_score, score)
-        self.avg_score = round(
-            new_avg(self.avg_score, score, self.games_played))
-        self.best_speed = max(self.best_speed, speed)
-        self.avg_speed = new_avg(self.avg_speed, speed, self.games_played)
-        self.games_played += 1
+    def __str__(self):
+        return self.displayed_name
 
 
 class SessionPlayerResult(models.Model):
     """Stores game session results per player"""
     session = models.ForeignKey('GameSession',
                                 on_delete=models.CASCADE,
-                                related_name='players')
-    player = models.ForeignKey('Player', on_delete=models.CASCADE,
+                                related_name='results')
+    player = models.ForeignKey('Player', on_delete=models.SET_NULL,
                                related_name='sessions', blank=True, null=True)
-    team = models.CharField(default='none', blank=True, max_length=255)
+    team = models.CharField(blank=True, max_length=50)
     score = models.IntegerField()
     speed = models.FloatField()
     mistake_ratio = models.FloatField()
-    winner = models.BooleanField()
+    is_winner = models.BooleanField()
     correct_words = models.PositiveIntegerField()
     incorrect_words = models.PositiveIntegerField()
 
@@ -111,14 +71,23 @@ class SessionPlayerResult(models.Model):
                 fields=('session', 'player'),
                 name='unique_players_for_game',
             ),
+            models.CheckConstraint(
+                check=Q(speed__gte=0),
+                name="non_negative_speed",
+            ),
+            models.CheckConstraint(
+                check=Q(mistake_ratio__gte=0),
+                name="non_negative_mistake_ratio",
+            )
         ]
 
 
 class GameSession(models.Model):
     """Stores information about sessions"""
     mode = models.CharField(max_length=1, choices=GameModes.choices)
-    name = models.CharField(max_length=50, unique=True)
-    # max_length==128 as in django.contrib.auth.models.User.password max_length
+    name = models.CharField(max_length=50, blank=True)
+    # max_length==128 since we use django.contrib.auth.hashers.make_password()
+    # same as for `password` field of `User` from django.contrib.auth.models
     password = models.CharField(max_length=128, blank=True)
     is_private = models.BooleanField(default=False, blank=True)
     players_max = models.PositiveIntegerField(default=0, blank=True)
@@ -153,6 +122,19 @@ class GameSession(models.Model):
         if self._state.adding and self.is_private:
             self.set_password(self.password)
         super().save(*args, **kwargs)
+
+    def save_results(self, results: list[dict]):
+        result_objs = [
+            SessionPlayerResult(session=self, **result)
+            for result in results
+        ]
+        for obj in result_objs:
+            obj.full_clean()
+        self.finished_at = timezone.now()
+        self.is_finished = True
+        with transaction.atomic():
+            SessionPlayerResult.objects.bulk_create(result_objs, batch_size=1000)
+            self.save()
 
     def set_password(self, password: str):
         self.password = make_password(password)

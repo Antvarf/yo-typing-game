463842eb32ed96bbf7078b81ef6f46881972dc93
Antvarf <antvarf@gmail.com>
2023-05-22 18:54:10 +0300

    wip/refactor: add working handler for `player_join`
    Commented out the unused functionality to limit the test scope, the refactored the rest to pass current tests.

--- base/helpers.py
+++ base/helpers.py
@@ -1,5 +1,6 @@
 import os
 import json
+import random
 
 from E.settings import BASE_DIR
 
@@ -9,7 +10,18 @@ def get_regular_words():
         words = json.loads(e.read())
     return words
 
+
 def get_yo_words():
     with open(os.path.join(BASE_DIR, "yo_words.json"), "r") as e:
         words = json.loads(e.read())
+    return words
+
+
+def get_words(n: int):
+    yo_words_cnt = int(n * 0.1)
+    words_cnt = n - yo_words_cnt
+
+    words = random.choices(get_regular_words(), k=words_cnt) \
+            + random.choices(get_yo_words(), k=yo_words_cnt)
+    random.shuffle(words)
     return words

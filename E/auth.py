efd83a3a5e157836f525c63c11068d5c286bedc7
Antvarf <antvarf@github.com>
2023-06-11 05:36:48 +0300

    wip/refactor: update jwt auth middleware + tests

--- E/auth.py
+++ E/auth.py
@@ -1,26 +1,33 @@
 from urllib.parse import parse_qs
 
+from channels.db import database_sync_to_async
 from django.contrib.auth.models import AnonymousUser
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.exceptions import AuthenticationFailed
 
 
+@database_sync_to_async
+def get_user_from_query_string(query_string: bytes):
+    authenticator = JWTAuthentication()
+    user = AnonymousUser
+
+    params = parse_qs(query_string.decode())
+    token = params.get('jwt', (None,))[0]
+    if token:
+        try:
+            raw_token = authenticator.get_raw_token(b"JWT " + token.encode())
+            validated_token = authenticator.get_validated_token(raw_token)
+            user = authenticator.get_user(validated_token)
+        except AuthenticationFailed:
+            pass
+
+    return user
+
+
 class JWTAuthMiddleware:
-    def __init__(self, inner):
-        self.inner = inner
+    def __init__(self, app):
+        self.app = app
 
-    def __call__(self, scope):
-        authenticator = JWTAuthentication()
-        user = AnonymousUser
-
-        params = parse_qs(scope["query_string"].decode())
-        token = params.get('jwt', (None,))[0]
-        if token:
-            try:
-                raw_token = authenticator.get_raw_token(b"JWT "+token.encode())
-                validated_token = authenticator.get_validated_token(raw_token)
-                user = authenticator.get_user(validated_token)
-            except AuthenticationFailed:
-                pass
-
-        return self.inner(dict(scope, user=user))
+    async def __call__(self, scope, receive, send):
+        scope['user'] = await get_user_from_query_string(scope['query_string'])
+        return await self.app(scope, receive, send)

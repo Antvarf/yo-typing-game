ecf5561007a08125f801f8fa16994f5d7b8cc46e
Antvarf <antvarf@gmail.com>
2023-06-28 19:08:34 +0300

    wip/refactor: attach new GameController to consumer

--- E/routing.py
+++ E/routing.py
@@ -14,44 +14,8 @@ application = ProtocolTypeRouter({
     "websocket": JWTAuthMiddleware(
         URLRouter([
             path(
-                "ws/play/dummy/<str:session_id>/<str:jwt>/",
-                consumers.BaseGameConsumer
-                ),
-            path(
-                "ws/play/single/<str:session_id>/<str:jwt>/",
-                consumers.SingleGameConsumer
-                ),
-            path(
-                "ws/play/endless/<str:session_id>/<str:jwt>/",
-                consumers.EndlessGameConsumer
-                ),
-            path(
-                "ws/play/tugofwar/<str:session_id>/<str:jwt>/",
-                consumers.TugOfWarGameConsumer
-                ),
-            path(
-                "ws/play/ironwall/<str:session_id>/<str:jwt>/",
-                consumers.IronWallGameConsumer
-                ),
-            path(
-                "ws/play/dummy/<str:session_id>",
-                consumers.BaseGameConsumer
-                ),
-            path(
-                "ws/play/single/<str:session_id>",
-                consumers.SingleGameConsumer
-                ),
-            path(
-                "ws/play/endless/<str:session_id>",
-                consumers.EndlessGameConsumer
-                ),
-            path(
-                "ws/play/tugofwar/<str:session_id>",
-                consumers.TugOfWarGameConsumer
-                ),
-            path(
-                "ws/play/ironwall/<str:session_id>",
-                consumers.IronWallGameConsumer
+                "ws/play/<str:session_id>/",
+                consumers.GameConsumer,
                 ),
             ])
         ),

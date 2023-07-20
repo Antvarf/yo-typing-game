591f6ca73beaa86b82231b03db45991e89a9f20e
Antvarf <antvarf@gmail.com>
2023-07-21 00:16:04 +0300

    chore: remove unnecessary files

--- deploy/redis.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo docker rm e_redis
-sudo docker run --name='e_redis' --network='host' -d redis
--- deploy/test.sh
+++
@@ -1,6 +0,0 @@
-cd /var/www/E
-git pull
-sudo docker build -t e_app .
-sudo docker run e_app bash -c "./manage.py makemigrations --noinput && ./manage.py migrate && ./manage.py test"
-sudo docker-compose build migrate
-sudo systemctl restart E_app
